05d3ff691cdbbcf36f3590beba24db09b91e1228
Greg Connell <gconnell@160over90.com>
2021-09-10 10:32:21 -0700

    componentizes settings class from wp-deploy-manager

--- index.js
+++ index.js
@@ -1,86 +1,33 @@
-const NamedRegExp = require('named-regexp-groups')
+const fs = require('fs')
+const requireFromString = require('require-from-string')
+const commentHooks = require('@ff0000-ad-tech/comment-hooks')
 
-/**
- * Parse a hook id
- *
- * [scope].[type].[param].[start/end]
- *
- */
-const parse = (hook) => {
-	// string will be parsed
-	if (typeof hook === 'string') {
-		const parts = hook.split('.')
-		return {
-			scope: parts[0],
-			type: parts[1],
-			param: parts[2]
+const debug = require('@ff0000-ad-tech/debug')
+const log = debug('index-settings')
+
+// read settings hook
+const load = (path) => {
+	const index = fs.readFileSync(path, 'utf8')
+	return read(index)
+}
+
+// define expected model with the hook-ids
+const INDEX_HOOKS = {
+	adParams: 'ad_params',
+	assets: 'assets'
+}
+const read = (source) => {
+	var settings = {}
+	for (var key in INDEX_HOOKS) {
+		var matches = source.match(commentHooks.getRegexForHook(`Red.Settings.${INDEX_HOOKS[key]}`))
+		if (matches) {
+			// settings hooks can be parsed with a little node-require trickery
+			settings[key] = requireFromString(`${matches.groups.content} module.exports = ${key};`)
 		}
 	}
-	// otherwise, assume hook is obj
-	return hook
-}
-
-/**
- * Stringify a hook object
- *
- *	{ scope, type, param }
- *
- */
-const stringify = (hook, { start, end, lang } = {}) => {
-	// ensure hook is object
-	hook = parseHook(hook)
-	let tag = `${hook.scope}.${hook.type}.${hook.param}`
-	if (start) {
-		tag = `${address}.start`
-	} else if (end) {
-		tag = `${address}.end`
-	}
-	if (lang === 'html') {
-		return `<!-- ${tag} -->`
-	} else if (lang === 'js') {
-		return `/*-- ${tag} --*/`
-	} else {
-		return tag
-	}
-}
-
-/**
- * Find hook content in source
- *
- */
-function getRegexForHook(hook) {
-	hook = parse(hook)
-	const scope = hook.scope == '*' ? '[^.]+' : hook.scope
-	const type = hook.type == '*' ? '[^.]+' : hook.type
-	const param = hook.param == '*' ? '[^.]+' : hook.param
-	return new NamedRegExp(
-		`(?<start>[^\n]+` +
-		`(?<scope>${scope}).` +
-		`(?<type>${type}).` +
-		`(?<param>${param}).start[^\n]+\n)` +
-		`(?<content>[^]*?)` +
-		`(?<end>[^S\n]*((/*--s*)|(<!--s*))?(?&scope).(?&type).(?&param).end[^\n]+)`
-	)
-}
-
-function getRegexForInsert(param, lang = 'js') {
-	param = param == '*' ? '[^s-]+' : param
-	if (lang == 'html') {
-		return new NamedRegExp('(<!)--s*Red.Insert.' + param + 's*--(>)')
-	} else {
-		return new NamedRegExp('(/*s*)--s*Red.Insert.' + param + 's*--(s**/)')
-	}
-}
-
-function getRegexForAll(scope) {
-	scope = scope || 'Red'
-	return new RegExp(`([^\n]+${scope}.[^.]+.[^.]+.[^\n]+\n)`, 'g')
+	return settings
 }
 
 module.exports = {
-	parse,
-	stringify,
-	getRegexForHook,
-	getRegexForInsert,
-	getRegexForAll
+	load
 }
